5dc11559b638e1ce7835b103a2af150d51294778
Jiri Koutny <jirkakoutny@gmail.com>
2019-11-02 10:16:29 +0100

    env-ini.sh must return correct exit code
    Better cache disable added

--- env-init.sh
+++ env-init.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -e
 
-function cleanup { rm ./env-init-functions.sh; }
+function cleanup {
+  rv=$?
+  rm ./env-init-functions.sh
+  exit $rv
+}
 trap cleanup EXIT
 
-curl "https://raw.githubusercontent.com/DataSentics/dev-env-init/master/env-init-functions.sh?$(date +%s)" --silent -o env-init-functions.sh
+curl "https://raw.githubusercontent.com/DataSentics/dev-env-init/master/env-init-functions.sh?$(date +%s)" -H 'Cache-Control: no-cache' --silent -o env-init-functions.sh
 . "env-init-functions.sh"
 
 prepare_environment_for_package
